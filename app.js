af8564e9a23ecd1a68ca488c0b4530c92d04ef74
Mahfuzur Rahman <mahfuz468865@gmail.com>
2021-11-02 07:01:46 +0600

    Days have been removed and re-factored accordingly

--- app.js
+++ app.js
@@ -8,30 +8,23 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 let presentDay = tempDate.getDay();
 
-
 //run the timer
 function countdownTimer() {
   const futureTime = +new Date(tempYear, tempMonth, tempDay, 12, 30, 00);
   const presentTime = +new Date();
   const difference = futureTime - presentTime;
 
-  if (difference <= 0) {
+  if (presentDay === 5 && difference <= 0) {
+    tempDay += 3;
+  } else if (difference <= 0) {
     tempDay++;
-  } else if (presentDay === 5) {
-    var hours = 71;
-    const minutes = Math.floor((difference / 1000 / 60) % 60);
-    const seconds = Math.floor((difference / 1000) % 60);
+  } else(difference > 0)
+  const hours = Math.floor(difference / (1000 * 60 * 60));
+  const minutes = Math.floor((difference / 1000 / 60) % 60);
+  const seconds = Math.floor((difference / 1000) % 60);
 
-    //Show the counter on UI
-    counter.innerHTML = `${hours} : ${minutes} : ${seconds}`;
-  } else {
-    var hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((difference / 1000 / 60) % 60);
-    const seconds = Math.floor((difference / 1000) % 60);
-
-    //Show the counter on UI
-    counter.innerHTML = `${hours} : ${minutes} : ${seconds}`;
-  }
+  //Show the counter on UI
+  counter.innerHTML = `${hours} : ${minutes} : ${seconds}`;
 
 }
 
